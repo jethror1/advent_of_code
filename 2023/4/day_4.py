cc10b3c328e22e786c10163f1cd2f741b28716d9
Jethro Rainford <rainford1995@gmail.com>
2024-11-03 11:37:17 +0000

    day 4 pt 1

---
+++ 2023/4/day_4.py
@@ -0,0 +1,22 @@
+import re
+import sys
+
+with open(sys.argv[1]) as fh:
+    contents = fh.read().rstrip("\n").splitlines()
+
+total = 0
+
+for line in contents:
+    winning, selected = line.split("|")
+    winning = set(re.findall(r"\d+", winning.split(":")[1]))
+    selected = set(re.findall(r"\d+", selected))
+    won = len(winning - (winning - selected))
+
+    if won == 0:
+        continue
+    elif won == 1:
+        total += 1
+    else:
+        total += 2 ** (won - 1)
+
+print(total)
